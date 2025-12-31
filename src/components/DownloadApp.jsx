abdb6b551a5bd6a060c9c619123b9868ddfa632b
Snofts <samuelsnofts@gmail.com>
2025-12-31 03:51:08 +0100

    Navbar bug fixed

--- src/components/DownloadApp.jsx
+++ src/components/DownloadApp.jsx
@@ -4,13 +4,13 @@ import { FaCheck } from "react-icons/fa";
 
 const DownloadApp = () => {
   return (
-    <section id="download-app" className="mt-40 px-10 flex flex-row justify-around items-center gap-4">
+    <section id="download-app" className="mt-40 px-10 grid grid-cols-2 grid-rows-1 justify-around items-center gap-4">
       <div>
         <h2 className="text-6xl font-semibold mb-10">Download APP</h2>
         <div className="flex justify-center flex-col w-full">
           {benefits.map((benefit) => (
             <div key={benefit.id} className="flex items-center gap-4 mb-4">
-              <div className={`${benefit.backgroundColor} w-7 h-7 flex justify-center items-center rounded-full`}>
+              <div className={`${benefit.backgroundColor} w-[2rem] h-[2rem] flex justify-center items-center rounded-full`}>
                 <FaCheck />
               </div>
               <p>{benefit.text}</p>
@@ -18,13 +18,13 @@ const DownloadApp = () => {
           ))}
         </div>
         <div className="flex items-center">
-          <img className="w-36 md:w-40 lg:w-44 hover:scale-105 transition-all duration-100" src="/Google-play.png" alt="Google Play Logo" />
-          <img className="w-40 md:w-44 hover:scale-105 transition-all duration-100" src="/Apple-store.png" alt="Apple Store Logo" />
+          <img className="w-36 md:w-40 object-cover lg:w-44 hover:scale-105 transition-all duration-100" src="/Google-play.png" alt="Google Play Logo" />
+          <img className="w-40 md:w-44 object-cover hover:scale-105 transition-all duration-100" src="/Apple-store.png" alt="Apple Store Logo" />
         </div>
       </div>
       {/* <div className="flex-1 flex justify-center">
       </div> */}
-      <img className="w-full max-w-md lg:max-w-lg object-contain" src="/iPhone-15-Pro-Max.png" alt="Iphone App Mockup" />
+      <img className="w-[50rem] h-[50rem] object-cotain lg:max-w-lg object-contain" src="/iPhone-15-Pro-Max.png" alt="Iphone App Mockup" />
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <div>
           <h2 className="text-white">PoliceRadioCodeApp</h2>
         </div>
-        <ul className={`${openNavigation ? "" : "hidden"}`}>
+        <ul className={`${openNavigation ? "" : "max-md:hidden"}`}>
           {navLinks.map(({ label, url }) => (
             <li
               key={label}
@@ -31,7 +31,7 @@ const Navbar = () => {
                   ? "text-[#40C4FF] scale-102 hr-2"
                   : ""
               }`}
-              onClick={() => setOpenNavigation(false)}
+              // onClick={() => setOpenNavigation(false)}
             >
               <a
                 // className="hover:text-blue hover:scale-100"
